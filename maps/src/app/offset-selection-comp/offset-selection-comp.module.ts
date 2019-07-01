7fcccf84855345fb85fe381bc3e85cd65c4b4ae2
RC Dev <rcdev198739@gmail.com>
2019-07-01 13:37:43 -0600

    Create a map-box component.

--- maps/src/app/offset-selection-comp/offset-selection-comp.module.ts
+++ maps/src/app/offset-selection-comp/offset-selection-comp.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OffsetSelectionCompComponent } from './offset-selection-comp.component';
+import { MapBoxComponent } from './map-box/map-box.component';
 
 @NgModule({
-  declarations: [OffsetSelectionCompComponent],
+  declarations: [OffsetSelectionCompComponent, MapBoxComponent],
   imports: [
     CommonModule
   ]
